3aff930d44c4384a25f73a1608312e55fc4e46d9
Henry Tseng <henrybear327@gmail.com>
2017-10-25 00:22:09 +0800

    Finished spliting for merge sort

--- Assignment/2/partition_sort.go
+++ Assignment/2/partition_sort.go
@@ -18,6 +18,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -26,14 +27,14 @@ type ProgramArgument struct {
 	outputFilename    *string
 	temporaryFilePath *string
 
-	chunkSize   int64
-	totalChunks *int64
+	chunkSize   int
+	totalChunks *int
 
 	useChunkSize          *bool
 	preserveInputFile     *bool
 	preserveTemporaryFile *bool
 
-	pivots []string
+	// pivots []string
 }
 
 var config ProgramArgument
@@ -46,7 +47,7 @@ func parseCommandLineArgument() {
 	config.outputFilename = flag.String("o", "out", "The file to store sorted result")
 	config.temporaryFilePath = flag.String("tmp", "/tmp", "The path for generated temporary files to be stored")
 
-	config.totalChunks = flag.Int64("chunks", 1024, "Minimal chunks to be created")
+	config.totalChunks = flag.Int("chunks", 1024, "Minimal chunks to be created")
 
 	config.preserveInputFile = flag.Bool("pi", false, "Set to true to preserve the input file")
 	config.preserveTemporaryFile = flag.Bool("pt", false, "Set to true to preserve the temporary file")
@@ -59,7 +60,7 @@ func parseCommandLineArgument() {
 	fmt.Println("input filename: " + *config.inputFilename)
 	fmt.Println("output filename: " + *config.outputFilename)
 	fmt.Println("temporary file path: " + *config.temporaryFilePath)
-	fmt.Println("total chunks to be created: " + strconv.FormatInt(*config.totalChunks, 10))
+	fmt.Println("total chunks to be created: " + strconv.FormatInt(int64(*config.totalChunks), 10))
 	fmt.Println("preserve input file: " + strconv.FormatBool(*config.preserveInputFile))
 	fmt.Println("preserve temporary file: " + strconv.FormatBool(*config.preserveTemporaryFile))
 	fmt.Printf("=================================================\n\n")
@@ -77,22 +78,27 @@ func openFile(filename string, errorString string) *os.File {
 	return file
 }
 
-func adjustChunkFactors(fileSize int64) {
+func adjustChunkFactors(fileSize int) {
 	fmt.Printf("\n=================================================\n")
 	if fileSize < *config.totalChunks {
 		*config.totalChunks = fileSize
-		fmt.Println("Total chunks to be created is updated to " + strconv.FormatInt(*config.totalChunks, 10))
+		fmt.Println("Total chunks to be created is updated to " + strconv.FormatInt(int64(*config.totalChunks), 10))
 	}
 
 	config.chunkSize = fileSize / *config.totalChunks
 	if fileSize%*config.totalChunks != 0 {
 		*config.totalChunks++
-		fmt.Println("Total chunks to be created is updated to " + strconv.FormatInt(*config.totalChunks, 10))
+		fmt.Println("Total chunks to be created is updated to " + strconv.FormatInt(int64(*config.totalChunks), 10))
 	}
 	fmt.Printf("The expected chunk size is %v kilobyte(s)\n", config.chunkSize)
 
-	config.pivots = make([]string, *config.totalChunks)
+	// config.pivots = make([]string, *config.totalChunks)
 	fmt.Printf("=================================================\n\n")
+}
+
+func openTempFile(index int) *os.File {
+	filename := "tmp_" + strconv.FormatInt(int64(index), 10)
+	return openFile(*config.temporaryFilePath+"/"+filename, "Opening tmpfile error")
 }
 
 func createTempFile(index int) *os.File {
@@ -122,10 +128,22 @@ func closeTempFile(file **os.File) {
 	*file = nil
 }
 
+func writeTempFile(buffer []string, chunkIndex int) {
+	sort.Strings(buffer)
+
+	tmpFileFd := createTempFile(chunkIndex)
+	for i := 0; i < len(buffer); i++ {
+		tmpFileFd.WriteString(buffer[i])
+	}
+	closeTempFile(&tmpFileFd)
+}
+
 func splitDataIntoChunks() {
 	fmt.Println("Split data into chunks")
 
 	/*
+		For merge sort...
+
 		- Open the input file
 		- Split the input file into k chunk files
 			- Record the first record of each chunk
@@ -137,37 +155,43 @@ func splitDataIntoChunks() {
 		panic(err)
 	}
 
-	adjustChunkFactors(stat.Size())
+	adjustChunkFactors(int(stat.Size()))
 
-	accumulatedSize := int64(0)
+	accumulatedSize := 0
 	chunkIndex := 0
-	var tmpFileFd *os.File
 
 	scanner := bufio.NewScanner(inputFile)
+
+	buffer := make([]string, 0)
 	for scanner.Scan() {
-		if tmpFileFd == nil {
-			tmpFileFd = createTempFile(chunkIndex)
-		}
+		// if tmpFileFd == nil {
+		// 	tmpFileFd = createTempFile(chunkIndex)
+		// }
 
 		str := scanner.Text()
 		str += "\n"
 		fmt.Printf("%v", str) // Println will add back the final '\n'
 
-		if accumulatedSize == 0 {
-			// this is the pivot
-			config.pivots[chunkIndex] = str
-		}
+		// if accumulatedSize == 0 {
+		// 	// this is the pivot
+		// 	config.pivots[chunkIndex] = str
+		// }
 
-		tmpFileFd.WriteString(str)
+		buffer = append(buffer, str)
+		// tmpFileFd.WriteString(str)
 
-		accumulatedSize += int64(len(str))
+		accumulatedSize += len(str)
 
 		if accumulatedSize >= config.chunkSize {
 			fmt.Printf("Accumulated bytes is %v\n", accumulatedSize)
-			closeTempFile(&tmpFileFd)
 			accumulatedSize = 0
 
+			sort.Strings(buffer)
+
+			writeTempFile(buffer, chunkIndex)
+
 			chunkIndex++
+			buffer = make([]string, 0)
 		}
 	}
 	if err := scanner.Err(); err != nil {
@@ -175,16 +199,20 @@ func splitDataIntoChunks() {
 	}
 	if accumulatedSize > 0 {
 		fmt.Printf("Accumulated bytes is %v\n", accumulatedSize)
-		closeTempFile(&tmpFileFd)
+
+		sort.Strings(buffer)
+
+		writeTempFile(buffer, chunkIndex)
+
 		chunkIndex++ // if accumulatedSize is 0, means the chunkIndex isn't used yet
 	}
 
-	*config.totalChunks = int64(chunkIndex)
-	fmt.Printf("\n===== pivot(s) ====\n")
-	for i := int64(0); i < *config.totalChunks; i++ {
-		fmt.Printf("%v", config.pivots[i])
-	}
-	fmt.Printf("================\n\n")
+	*config.totalChunks = chunkIndex
+	// fmt.Printf("\n===== pivot(s) ====\n")
+	// for i := 0; i < *config.totalChunks; i++ {
+	// 	fmt.Printf("%v", config.pivots[i])
+	// }
+	// fmt.Printf("================\n\n")
 }
 
 func sortChunks() {
@@ -196,6 +224,10 @@ func sortChunks() {
 		- Read file
 		- Sort file
 	*/
+
+	// for i := 0; i < *config.totalChunks; i++ {
+	// 	tmpFile := openTempFile(i)
+	// }
 }
 
 func mergeChunks() {
