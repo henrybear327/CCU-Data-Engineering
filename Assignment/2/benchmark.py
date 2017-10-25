a413910f6a9a3bb01bb6b77c4d1aeb10a020d4ff
Henry Tseng <henrybear327@gmail.com>
2017-10-25 21:20:54 +0800

    Added banchmark.py

--- Assignment/2/benchmark.py
+++ Assignment/2/benchmark.py
@@ -5,7 +5,7 @@ import pathlib
 from subprocess import run
 
 def time_bash_sort(inp, out):
-    print("Timing system sort using {0} as input and {1} as output".format(inp, out))
+    print("\nTiming system sort using {0} as input and {1} as output".format(inp, out))
     cmd = "sort -d {0} -o {1}".format(inp, out)
 
     start_time = time.time()
@@ -13,42 +13,30 @@ def time_bash_sort(inp, out):
     end_time = time.time()
     return end_time - start_time
 
-def time_my_sort(inp, out, p, type):
+def time_my_sort(inp, out, chunks):
     print("Build my go sorting code...")
     build = "go build -o my_sort *.go"
     run(build, shell=True)
     print("Done")
 
-    cmd = './my_sort -i {} -o {} -p {} -a {}'.format(inp, out, p, type)
+    print("\nTiming my sort using {0} as input and {1} as output".format(inp, out))
+    cmd = './my_sort -i {0} -o {1} -chunks {2} -tmp {3}'.format(inp, out, chunks, "./tmp")
     start_time = time.time()
     run(cmd, shell=True)
     end_time = time.time()
     return end_time - start_time
 
-folder = pathlib.Path('/tmp/hw2/')
-folder.mkdir(exist_ok=True)
-inps = sorted(pathlib.Path('../data/C_Chat/').glob('*00'))
+def check(out):
+    cmd = "sort -d -c {0}".format(out)
+    run(cmd, shell=True)
 
-results = []
-for inp in inps:   
-    for p in [100_000 * (1 << exp) for exp in range(5)]:
-        out = './out.{}'.format(inp.stem)
+in_file = "./testcase/number.in"
+out_file = "./testcase/number.out"
+my_out_file = "number.my.out"
 
-        print('*' * 40)
-        print('inp =', inp)
-        print('out =', out)
-        print('p =', p)
-        print('*' * 40)
+time_bash_sort(in_file, out_file)
+time_my_sort(in_file, my_out_file, 1000)
 
-        res1 = time_alg(inp, out, p, 'parti')
-        results.append({'size': inp.stem, 'type': 'parti', 'p': p, 'dur': res1})
-
-        res2 = time_alg(inp, out, p, 'merge')
-        results.append({'size': inp.stem, 'type': 'merge', 'p': p, 'dur': res2})
-
-        with open('./result.csv', 'w') as f:
-            fieldnames = ['size', 'type', 'p', 'dur']
-            writer = csv.DictWriter(f, fieldnames=fieldnames)
-            writer.writeheader()
-            for row in results:
-                writer.writerow(row)
+print("\n\n=====================================")
+check(my_out_file)
+print("=====================================")
