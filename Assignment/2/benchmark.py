0b9a2788140f054ef39237b6312294b4e1bfe432
Henry Tseng <henrybear327@gmail.com>
2017-10-25 21:35:44 +0800

    Iterate over a list for filename

--- Assignment/2/benchmark.py
+++ Assignment/2/benchmark.py
@@ -4,6 +4,11 @@ import time
 import pathlib
 from subprocess import run
 
+in_file = "./testcase/number_{0}M.in"
+out_file = "./testcase/number_{0}M.out"
+my_out_file = "number.my.out"
+fileList = ['50', '100', '200', '300', '400', '500', '600']
+
 def time_bash_sort(inp, out):
     print("\nTiming system sort using {0} as input and {1} as output".format(inp, out))
     cmd = "sort -d {0} -o {1}".format(inp, out)
@@ -26,16 +31,13 @@ def time_my_sort(inp, out, chunks):
     end_time = time.time()
     return end_time - start_time
 
-def check(out):
-    cmd = "sort -d -c {0}".format(out)
+def check(my, ans):
+    cmd = "cmp {0} {1}".format(my, ans)
     run(cmd, shell=True)
 
-in_file = "./testcase/number.in"
-out_file = "./testcase/number.out"
-my_out_file = "number.my.out"
-
-time_bash_sort(in_file, out_file)
-time_my_sort(in_file, my_out_file, 1000)
+for i in fileList:
+    time_bash_sort(in_file.format(i), out_file.format(i))
+    time_my_sort(in_file.format(i), my_out_file, 1000)
 
 print("\n\n=====================================")
 check(my_out_file)
