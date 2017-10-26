2b23be962fe30afcc596e332fdd19badf7d37195
Henry Tseng <henrybear327@gmail.com>
2017-10-26 14:08:29 +0800

    Added debug messages

--- Assignment/2/baseline/main.cpp
+++ Assignment/2/baseline/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    printf("Reading...\n");
     FILE* fd = fopen("../testcase/number_500M.in", "r");
     vector<string> inp;
     char buffer[40000];
@@ -13,12 +14,15 @@ int main()
         inp.push_back(buffer);
     }
 
+    printf("Sorting...\n");
     sort(inp.begin(), inp.end());
 
+    printf("Writing...\n");
     FILE* ans = fopen("../testcase/number_500M.baseline.out", "w");
     for(auto i : inp) {
         fprintf(ans, "%s\n", i.c_str());
     }
 
+    printf("Ends!\n");
     return 0;
 }
