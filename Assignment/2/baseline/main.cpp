c6437d65154f05623b0ffe41584f65612511fa2b
Henry Tseng <henrybear327@gmail.com>
2017-10-26 14:07:25 +0800

    Add baseline.cpp

--- Assignment/2/baseline/main.cpp
+++ Assignment/2/baseline/main.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main()
 {
+    FILE* fd = fopen("../testcase/number_500M.in", "r");
+    vector<string> inp;
+    char buffer[40000];
+    while(fgets(buffer, 40000, fd) != NULL) {
+        inp.push_back(buffer);
+    }
 
-    return 0
+    sort(inp.begin(), inp.end());
+
+    FILE* ans = fopen("../testcase/number_500M.baseline.out", "w");
+    for(auto i : inp) {
+        fprintf(ans, "%s\n", i.c_str());
+    }
+
+    return 0;
 }
