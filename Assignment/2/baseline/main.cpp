81e4e8002d63433c7001cfde1810ab4e997d393a
Henry Tseng <henrybear327@gmail.com>
2017-10-26 14:14:45 +0800

    Update baseline code with timer

--- Assignment/2/baseline/main.cpp
+++ Assignment/2/baseline/main.cpp
@@ -1,28 +1,89 @@
 #include <bits/stdc++.h>
+#include <sys/time.h>
 
 using namespace std;
 
 #define tmp_files 1000
 
+void run()
+{
+    vector<string> inp;
+
+    {
+        struct timeval t1, t2;
+        double elapsedTime;
+
+        // start timer
+        gettimeofday(&t1, NULL);
+
+        printf("Reading...\n");
+        FILE *fd = fopen("../testcase/number_500M.in", "r");
+        char buffer[40000];
+        while (fgets(buffer, 40000, fd) != NULL) {
+            inp.push_back(buffer);
+        }
+
+        // stop timer
+        gettimeofday(&t2, NULL);
+
+        // compute and print the elapsed time in millisec
+        elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;    // sec to ms
+        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+        printf("%f ms\n", elapsedTime);
+
+        fclose(fd);
+    }
+
+    {
+        struct timeval t1, t2;
+        double elapsedTime;
+
+        // start timer
+        gettimeofday(&t1, NULL);
+
+        printf("Sorting...\n");
+        sort(inp.begin(), inp.end());
+
+        // stop timer
+        gettimeofday(&t2, NULL);
+
+        // compute and print the elapsed time in millisec
+        elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;    // sec to ms
+        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+        printf("%f ms\n", elapsedTime);
+    }
+
+    {
+        struct timeval t1, t2;
+        double elapsedTime;
+
+        // start timer
+        gettimeofday(&t1, NULL);
+
+        printf("Writing...\n");
+        FILE *ans = fopen("../testcase/number_500M.baseline.out", "w");
+        for (auto i : inp) {
+            fprintf(ans, "%s\n", i.c_str());
+        }
+
+        printf("Ends!\n");
+
+        // stop timer
+        gettimeofday(&t2, NULL);
+
+        // compute and print the elapsed time in millisec
+        elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;    // sec to ms
+        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+        printf("%f ms\n", elapsedTime);
+
+        fclose(ans);
+    }
+}
+
 int main()
 {
-    printf("Reading...\n");
-    FILE* fd = fopen("../testcase/number_500M.in", "r");
-    vector<string> inp;
-    char buffer[40000];
-    while(fgets(buffer, 40000, fd) != NULL) {
-        inp.push_back(buffer);
-    }
+    run();
+    run();
 
-    printf("Sorting...\n");
-    sort(inp.begin(), inp.end());
-
-    printf("Writing...\n");
-    FILE* ans = fopen("../testcase/number_500M.baseline.out", "w");
-    for(auto i : inp) {
-        fprintf(ans, "%s\n", i.c_str());
-    }
-
-    printf("Ends!\n");
     return 0;
 }
