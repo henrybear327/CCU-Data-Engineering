26755ffc0f9819b0e3aa86222cd061ec4fa06133
Henry Tseng <henrybear327@gmail.com>
2017-11-02 17:26:41 +0800

    Save space

--- Assignment/2/benchmark.sh
+++ Assignment/2/benchmark.sh
@@ -5,12 +5,18 @@ go build -o my_sort *.go
 mkdir -p /tmp/testcases/my
 mkdir -p /tmp/testcases/my/log
 
+CHUNKS=1000
+DEPTH=4
+
 for (( i=1; i<=1000000000; i*=10))
 do
 	echo "generating $i.in"
-	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.nosort.out -chunks 100 -depth=6 -freq=1 -p=0) 2>&1 | tee /tmp/testcases/my/log/$i.nosort.out.output
-	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.single.out -chunks 100 -depth=6 -freq=1 -p=1 && sort -c /tmp/testcases/my/$i.single.out) 2>&1 | tee /tmp/testcases/my/log/$i.single.out.output
-	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.parallel.out -chunks 100 -depth=6 -freq=1 -p=2 && sort -c /tmp/testcases/my/$i.parallel.out) 2>&1 | tee /tmp/testcases/my/log/$i.parallel.out.output
+	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.nosort.out -chunks $CHUNKS -depth=$DEPTH -freq=1 -p=0) 2>&1 | tee /tmp/testcases/my/log/$i.nosort.out.output
+	rm /tmp/testcases/my/$i.nosort.out
+	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.single.out -chunks $CHUNKS -depth=$DEPTH -freq=1 -p=1 && sort -c /tmp/testcases/my/$i.single.out) 2>&1 | tee /tmp/testcases/my/log/$i.single.out.output
+	rm /tmp/testcases/my/$i.single.out
+	(time ./my_sort -i /tmp/testcases/$i.in -o /tmp/testcases/my/$i.parallel.out -chunks $CHUNKS -depth=$DEPTH -freq=1 -p=2 && sort -c /tmp/testcases/my/$i.parallel.out) 2>&1 | tee /tmp/testcases/my/log/$i.parallel.out.output
+	rm /tmp/testcases/my/$i.parallel.out
 	echo "done!"
 done
 
