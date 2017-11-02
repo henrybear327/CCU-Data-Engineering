931d8ab860c702d263db52d522a47e774823073a
Henry Tseng <henrybear327@gmail.com>
2017-11-02 10:46:38 +0800

    Finish basic version for test1

---
+++ test/1/main.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define BUFFER_SIZE 10000000
+
+char buffer[BUFFER_SIZE];
+const char *delimiter = "\r\n ";
+
+map<string, int> cnt;
+typedef pair<int, string> ii;
+
+struct classcomp {
+    bool operator()(const ii &lhs, const ii &rhs) const
+    {
+        if (lhs.first == rhs.first)
+            return lhs.second < rhs.second;
+        return lhs.first > rhs.first;
+    }
+};
+set<ii, classcomp> ans;
+
+int main()
+{
+    while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+        char *tok = strtok(buffer, delimiter);
+        while (tok != NULL) {
+            cnt[tok]++;
+
+            tok = strtok(NULL, delimiter);
+        }
+    }
+
+    for (auto i : cnt) {
+        // printf("%20s %10d\n", i.first.c_str(), i.second);
+        ans.insert(ii(i.second, i.first));
+    }
+
+    for (auto i : ans)
+        printf("%20s %10d\n", i.second.c_str(), i.first);
+
+    return 0;
+}
